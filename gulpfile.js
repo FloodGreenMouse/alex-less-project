382cb3e09728054542551baae5c4a210845e4338
Sergey <sergeypltinfo@gmail.com>
2021-10-15 23:36:32 +0300

    add less, browserSync, watchers

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,29 @@
 const gulp = require('gulp')
 const browserSync = require('browser-sync').create()
+const less = require('gulp-less')
+const rename = require('gulp-rename')
 
 gulp.task('browserSync', () => {
-  browserSync.reload()
-})
-
-gulp.task('default', () => {
   browserSync.init({
     server: {
       baseDir: './src'
     }
   })
+})
 
+gulp.task('less', () => {
+  return gulp.src('./src/less/index.less')
+    .pipe(less())
+    .pipe(rename('index.css'))
+    .pipe(gulp.dest('./src/styles'))
+    .pipe(browserSync.stream())
+})
+
+gulp.task('watch', () => {
   gulp.watch('./src/*.html', gulp.series('browserSync'))
   gulp.watch('./src/*/*.html', gulp.series('browserSync'))
+  gulp.watch('./src/less/*.less', gulp.series('less'))
+  gulp.watch('./src/less/*/*.less', gulp.series('less'))
 })
+
+gulp.task('default', gulp.parallel('less', 'browserSync', 'watch'))
